f4f7efc31b9bc89bf63094cfdc59a4d5c00ab221
Samantha Lustig <sl2477@cornell.edu>
2019-04-23 12:23:07 -0400

    added forms to events page

--- events.php
+++ events.php
@@ -18,16 +18,49 @@ $title = "EVENTS"
   <!-- TODO: This should be your main page for your site. -->
   <?php include("includes/header.php"); ?>
 
-  <!--Form to add events -->
+  <!-- Form to update events -->
+  <h2>Update an Event?</h2>
   <fieldset>
-    <form id = "add_events" action = "events.php" method = "post">
-      <label for = "image_file">Add an image: </label>
-      <input id = "image_file" type = "file" name = "image_file">
-      <button name = "upload_image" type = "submit">Upload</button>
+    <form id = "update_events" action = "events.php" method = "post">
+      <label for = "update_event">Select an Event: </label>
+      <!-- Will change this to a drop down later-->
+      <input id = "update_event" type = "text" name = "update_event">
+      <label for = "update_date">Change Date: </label>
+      <input id = "update_date" type = "text" name = "update_date">
+      <label for = "update_time"> Change Time: </label>
+      <input id = "update_time" type = "text" name = "update_time">
+      <label for = "update_location"> Change Location: </label>
+      <input id = "update_location" type = "text" name = "update_location">
+      <button name = "update_event" type = "submit">Update</button>
     </form>
   </fieldset>
-  <?php include("includes/footer.php"); ?>
 
+  <!--Form to remove events -->
+  <h2>Delete an Event?</h2>
+  <fieldset>
+    <form id = "delete_events" action = "events.php" method = "post">
+      <label for = "delete_event">Select an Event: </label>
+      <!-- Will change this to a drop down later-->
+      <input id = "delete_event" type = "text" name = "delete_event">
+      <button name = "delete_event" type = "submit">Delete</button>
+    </form>
+  </fieldset>
+
+  <!--Form to add events -->
+  <h2>Add an Event?</h2>
+  <fieldset>
+    <form id = "add_events" action = "events.php" method = "post">
+      <label for = "new_name"> Event Name: </label>
+      <input id = "new_name" type = "text" name = "new_name">
+      <label for = "new_date">Date: </label>
+      <input id = "new_date" type = "text" name = "new_date">
+      <label for = "new_time">Time: </label>
+      <input id = "new_time" type = "text" name = "new_time">
+      <label for = "new_location">Location: </label>
+      <input id = "new_location" type = "text" name = "new_location">
+      <button name = "add_event" type = "submit">Add</button>
+    </form>
+  </fieldset>
   <?php include("includes/footer.php"); ?>
 </body>
 </html>
