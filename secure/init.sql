79912c1b66dd3c10e4a6b1dc262da4214195fb70
Clive Anthony Duncan <cad324@cornell.edu>
2019-04-25 20:43:54 -0400

    added table and seed data  - for users, messages and mail list

--- secure/init.sql
+++ secure/init.sql
@@ -2,8 +2,46 @@
 BEGIN TRANSACTION;
 
 -- TODO: create tables
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `username` TEXT NOT NULL,
+    `password` TEXT,
+    `eboard` TEXT
+);
+
+CREATE TABLE `messages` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `name` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `message` TEXT NOT NULL
+);
+
+CREATE TABLE `mail_list` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `email` TEXT NOT NULL
+);
+
 
 -- TODO: initial seed data
+
+-- USERS
+INSERT INTO users(username, password, eboard) VALUES("bryant1", "$2y$10$UI2nbXH1JBs22rU97jl5XeFTBeeqdLWEieoH4XvY0FfyN65Sh3kB2", yes);
+-- password: monkey
+INSERT INTO users(username, password) VALUES("sharon10", "$2y$10$t7UtHE1yqUJJ2OGRel/4X.pqGik1jSwGF86x30xNadJLfJ1SYCqFi");
+-- password: dolphin
+
+-- MESSAGES
+INSERT INTO messages(name, email, message) VALUES("Clive", "cad324@cornell.edu",
+        "Please tell me more about your club");
+INSERT INTO messages(name, email, message) VALUES("John Doe", "jd11@gmail.com",
+        "How do I become a part of this club");
+
+-- MAIL_LIST
+INSERT INTO mail_list(email) VALUES("cad324@cornell.edu");
+INSERT INTO mail_list(email) VALUES("gf123@cornell.edu");
+INSERT INTO mail_list(email) VALUES("jim@gmail.com");
+INSERT INTO mail_list(email) VALUES("ww90@cornell.edu");
+INSERT INTO mail_list(email) VALUES("willy@yahoo.com");
 
 -- TODO: FOR HASHED PASSWORDS, LEAVE A COMMENT WITH THE PLAIN TEXT PASSWORD!
 
