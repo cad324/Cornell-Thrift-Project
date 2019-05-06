6cc884a141ae9b486d07b563f95e08ca7a68a119
Ha Yeon Shin <hs637@cornell.edu>
2019-05-06 17:22:52 -0400

    hs

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,10 @@
 <header>
   <div class="navbar" id="myHeader">
-    <a id="logo_a" href="index.php"><img class="logo" src="images/logo.png" alt="logo" /></a>
     <nav id="nav">
       <a class="home_link" href="index.php">Home</a>
       <a href="about.php">Members</a>
       <a href="events.php">Events</a>
-      <div class="dropdown">
-        <button class="dropbtn">Resources
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="resources_stores.php">Thrift Stores</a>
-          <a href="resources_tip.php">Thrift Tips</a>
-        </div>
-      </div>
+      <a href="resources_stores.php">Stores Near By</a>
       <a href="contact.php">Contact Us</a>
       <?php if (!is_user_logged_in()) { ?>
         <form action="login.php" >
