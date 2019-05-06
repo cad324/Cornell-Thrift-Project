162888023565168c908e299aeaa79fa37d893ee3
Clive Anthony Duncan <cad324@cornell.edu>
2019-05-06 18:08:21 -0400

    login button added

--- includes/header.php
+++ includes/header.php
@@ -8,9 +8,10 @@
       <a href="resources_stores.php">Stores Near By</a>
       <a href="contact.php">Contact Us</a>
       <?php if (!is_user_logged_in()) { ?>
-        <form action="login.php" >
+        <!-- <form action="login.php" >
           <input type="submit" id="login_btn" value="Login">
-        </form>
+        </form> -->
+        <a id="login_btn" href="login.php">Login</a>
       <?php } else { ?>
         <form method="get" action="index.php" >
           <input type="submit" name="logout" id="login_btn" value="Logout"/>
